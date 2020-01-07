e093f77dcb0e232d8961e7dd389f34ebaabfa1ec
Nick <Nick@192.168.0.22>
2020-01-06 21:14:24 -0500

    Project explorer file loading

--- CycloneIDE/src/display/ProjectExplorer.java
+++ CycloneIDE/src/display/ProjectExplorer.java
@@ -1,11 +1,16 @@
 package display;
 
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+
+import utils.Project;
 
 public class ProjectExplorer extends Perspective{
 	
@@ -14,27 +19,81 @@ public class ProjectExplorer extends Perspective{
 	private static int width = (int) (Perspective.screenWidth/4 - 25);
 	private static int height = (int) (Perspective.screenHeight - 65) - 25;
 	
-	private JTextArea consoleTextArea = new JTextArea();
+	private JPanel projectExplorerPanel = new JPanel();//Main panel that contains all project panels- Box Layout
+	private ArrayList<String> projectDirectories = new ArrayList<String>();
+	private ArrayList<Project> projects = new ArrayList<Project>();
+	//Each project has its own main panel - Box Layout
+	//This panel will contain:
+	//	-Button that when pressed reveals project files
+	//	-Project file buttons that appear and disappear when project file is clicked
+	
 	
 	public ProjectExplorer() {
 		
 		panelSetup(orginX, orginY, (int) Perspective.screenWidth, (int) Perspective.screenHeight, new Color(243, 243, 243));
 		
 		addJComponents();
-		
+		loadFiles();
+		createProjects();
 	}
 	
 	public void addJComponents() {
-		consoleTextArea.setFont(new Font("Gill Sans MT Condensed", Font.PLAIN, 20));
-		consoleTextArea.setBounds(0, 0, width, height);
-		consoleTextArea.setLineWrap(true);
-		consoleTextArea.setWrapStyleWord(true);
-		JScrollPane consoleTextAreaScroll = new JScrollPane(consoleTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		
+		projectExplorerPanel.setLayout(new BoxLayout(projectExplorerPanel, BoxLayout.Y_AXIS));
+		JScrollPane consoleTextAreaScroll = new JScrollPane(projectExplorerPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		consoleTextAreaScroll.setBounds(0 , 0, width, height);
 		add(consoleTextAreaScroll); 
 		
 	}
-
+	
+	//This method loads the project files
+	private void loadFiles() {
+		
+		//Source: https://stackoverflow.com/questions/5125242/java-list-only-subdirectories-from-a-directory-not-files/5125258
+		File file = new File("projects");
+		String[] directories = file.list(new FilenameFilter() {
+			@Override
+			public boolean accept(File current, String name) {
+				return new File(current, name).isDirectory();
+			}
+		});
+		
+		//Add the directories to the project directories array list
+		for(String project: directories) {
+			projectDirectories.add(project);
+		}
+		
+		//Print the names of each project
+		System.out.println("Projects: ");
+		for(String project: projectDirectories) {
+			System.out.println(project);
+		}
+		
+	}
+	
+	//This method adds the buttons for each project to the project manager
+	private void createProjects() {
+		
+		//Loop through the project directory array and create a project for each directory
+		for(int i = 0; i < projectDirectories.size(); i++) {
+			
+			Project project = new Project(projectDirectories.get(i));
+			projects.add(project);
+			projectExplorerPanel.add(project.getProjectPanel());
+			
+		}
+		
+	}
+	
+	//This method removes all components from the project explorer
+	private void removeAllComponents() {
+		
+		projectExplorerPanel.removeAll();
+		projectDirectories.clear();
+		projects.clear();
+		
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
---
+++ CycloneIDE/src/utils/Project.java
@@ -0,0 +1,134 @@
+package utils;
+
+import java.awt.Color;
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import display.Perspective;
+
+public class Project {
+	
+	private static int width = (int) (Perspective.screenWidth/4 - 25);
+	private static int buttonHeight = 50;
+	
+	//Each project has its own main panel - Box Layout
+	//This panel will contain:
+	//	-Button that when pressed reveals project files
+	//	-Project file buttons that appear and disappear when project file is clicked
+	private JPanel projectPanel = new JPanel();
+	private JButton projectButton = new JButton();
+	private JPanel filePanel = new JPanel(); //Within the main panel and stores buttons for each project file
+	private ArrayList<JButton> fileButtons = new ArrayList<JButton>(); //Contains buttons for each file within the project
+	
+	private String projectName;
+	private File filepath; 
+	
+	public Project(String projectName) {
+		
+		this.projectName = projectName;
+		this.filepath = new File(String.format("projects/%s", projectName));
+		
+		setupJComponents();
+		setupFileButtons();
+		
+	}
+	
+	//This method sets up the panels and project button
+	private void setupJComponents() {
+		
+		//Setup the main panel
+		projectPanel.setLayout(new BoxLayout(projectPanel, BoxLayout.Y_AXIS));
+		projectPanel.setBounds(0, 0, width, (filepath.listFiles().length + 1) * buttonHeight);
+		projectPanel.setMaximumSize(projectPanel.getSize());
+		projectPanel.setMinimumSize(projectPanel.getSize());
+		projectPanel.setPreferredSize(projectPanel.getSize());
+		//projectPanel.setBorder(BorderFactory.createLineBorder(Color.yellow, 5));
+		
+		//Setup the folder button
+		projectButton.setSize(width, buttonHeight);
+		projectButton.setMaximumSize(projectButton.getSize());
+		projectButton.setMinimumSize(projectButton.getSize());
+		projectButton.setPreferredSize(projectButton.getSize());
+		projectButton.setText(projectName);
+		projectPanel.add(projectButton);
+		
+		//Setup the file panel
+		filePanel.setLayout(null);
+		filePanel.setBounds(0, 50, width, filepath.listFiles().length * buttonHeight);
+		projectPanel.add(filePanel);
+		
+	}
+	
+	//This method sets up the buttons used to access individual files within the project
+	private void setupFileButtons() {
+		
+		//Loop through the project directory array and create a project for each directory
+		for(int i = 0; i < filepath.listFiles().length; i++) {
+			
+			System.out.println("file: " +filepath.listFiles()[i].getName());
+			JButton fileButton = new JButton(filepath.listFiles()[i].getName());
+			fileButton.setBounds(width / 10, i * buttonHeight, width * 9 / 10, buttonHeight);
+			fileButton.setMaximumSize(fileButton.getSize());
+			fileButton.setMinimumSize(fileButton.getSize());
+			fileButton.setPreferredSize(fileButton.getSize());
+			filePanel.add(fileButton);
+			fileButtons.add(fileButton);
+			
+		}
+		
+	}
+	
+	public JPanel getProjectPanel() {
+		return projectPanel;
+	}
+
+	public void setProjectPanel(JPanel projectPanel) {
+		this.projectPanel = projectPanel;
+	}
+
+	public JButton getProjectButton() {
+		return projectButton;
+	}
+
+	public void setProjectButton(JButton projectButton) {
+		this.projectButton = projectButton;
+	}
+
+	public JPanel getFilePanel() {
+		return filePanel;
+	}
+
+	public void setFilePanel(JPanel filePanel) {
+		this.filePanel = filePanel;
+	}
+
+	public ArrayList<JButton> getFileButtons() {
+		return fileButtons;
+	}
+
+	public void setFileButtons(ArrayList<JButton> fileButtons) {
+		this.fileButtons = fileButtons;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public File getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(File filepath) {
+		this.filepath = filepath;
+	}
+
+}
