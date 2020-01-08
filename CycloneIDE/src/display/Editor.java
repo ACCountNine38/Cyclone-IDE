8989159dac43f177ac6e91ee069eaf85d613b747
072921778 <072921778@402X1066HF0FFL2.EDU.YRDSB.NET>
2020-01-08 13:30:34 -0500

    tabs

--- CycloneIDE/src/display/Editor.java
+++ CycloneIDE/src/display/Editor.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -19,9 +20,11 @@ public class Editor extends Perspective {
 	private static int width = (int) (Perspective.screenWidth/4*3 - 50);
 	private static int height = (int) (Perspective.screenHeight/3*2 - 25);
 	
-	private JTextArea consoleTextArea = new JTextArea();
-	private LineNumberModelImpl lineNumberModel = new LineNumberModelImpl(consoleTextArea);
+	private JTextArea editorTextArea = new JTextArea();
+	private LineNumberModelImpl lineNumberModel = new LineNumberModelImpl(editorTextArea);
 	private LineNumberComponent lineNumberComponent = new LineNumberComponent(lineNumberModel);
+	
+	private JTabbedPane tabbedPane = new JTabbedPane();
 	
 	public Editor() {
 		
@@ -33,11 +36,15 @@ public class Editor extends Perspective {
 	
 	public void addJComponents() {
 		
-		consoleTextArea.setFont(new Font("serif", Font.PLAIN, 22));
-		consoleTextArea.setBounds(0, 0, width, height);
-		consoleTextArea.setLineWrap(true);
-		consoleTextArea.setWrapStyleWord(true);
-		consoleTextArea.getDocument().addDocumentListener(new DocumentListener(){
+		//Setup tabbed pane
+		tabbedPane.setBounds(0, 0, width, height);
+		add(tabbedPane);
+		
+		editorTextArea.setFont(new Font("serif", Font.PLAIN, 22));
+		editorTextArea.setBounds(0, 0, width, height);
+		editorTextArea.setLineWrap(true);
+		editorTextArea.setWrapStyleWord(true);
+		editorTextArea.getDocument().addDocumentListener(new DocumentListener(){
 
 			@Override
 			public void changedUpdate(DocumentEvent arg0) {
@@ -62,11 +69,37 @@ public class Editor extends Perspective {
 
 		});
 		
-		JScrollPane consoleTextAreaScroll = new JScrollPane(consoleTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		JScrollPane consoleTextAreaScroll = new JScrollPane(editorTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		consoleTextAreaScroll.setBounds(0 , 0, width, height);
 		consoleTextAreaScroll.setRowHeaderView(lineNumberComponent);
-		add(consoleTextAreaScroll);
+		//consoleTextAreaScroll.setViewportView(editorTextArea);
+		//add(consoleTextAreaScroll);
 		
+		tabbedPane.add(consoleTextAreaScroll);
+		
+	}
+	
+	//This method creates a new tab for a class file
+	public void addTab(String projectName, String className) {
+		
+		
+		
+	}
+
+	public JTextArea getEditorTextArea() {
+		return editorTextArea;
+	}
+
+	public void setEditorTextArea(JTextArea editorTextArea) {
+		this.editorTextArea = editorTextArea;
+	}
+
+	public JTabbedPane getTabbedPane() {
+		return tabbedPane;
+	}
+
+	public void setTabbedPane(JTabbedPane tabbedPane) {
+		this.tabbedPane = tabbedPane;
 	}
 
 	@Override
