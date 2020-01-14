8113a7cb8edebd26f2f10d34a7569c6d3c0749ca
Nick <Nick@192.168.0.10>
2020-01-14 18:19:14 -0500

    c

---
+++ CycloneIDE/src/JarRunFile.java
@@ -0,0 +1,10 @@
+public class JarRunFile {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+public static void execute() {
+		
+		main(null);
+		
+	}
+}
